287b70720b7d040e48cc54f6885057a2c3f0456c
Natanael Rio Wijaya <gafterlife@gmail.com>
2026-02-04 14:02:10 +0700

    rio

--- src/app/api/p/sumView/route.js
+++ src/app/api/p/sumView/route.js
@@ -1,19 +1,37 @@
 import { prisma } from "@/controllers/prisma";
-import { ResponseData } from '@/components/api/ResponseData'
+import { ResponseData } from "@/components/api/ResponseData";
 
 export async function GET(req) {
-    const authorization = req.headers.get('authorization')
+    const authorization = req.headers.get("authorization");
 
-    if (authorization == process.env.NEXT_PUBLIC_SECREET) {
-        const data = await prisma.listProduct.aggregate({
+    if (authorization === process.env.NEXT_PUBLIC_SECREET) {
+
+        const product = await prisma.listProduct.aggregate({
             _sum: {
                 viewProduct: true,
             },
         });
-        const res = await ResponseData(data, authorization)
-        return res
+
+        const artikel = await prisma.postArtikel.aggregate({
+            _sum: {
+                viewArtikel: true, // sesuaikan dengan nama field di schema
+            },
+        });
+
+        const data = {
+            productView: product._sum.viewProduct ?? 0,
+            artikelView: artikel._sum.viewArtikel ?? 0,
+            totalView:
+                (product._sum.viewProduct ?? 0) +
+                (artikel._sum.viewArtikel ?? 0),
+        };
+
+        return ResponseData(data, authorization);
     }
 
-    return Response.json({ status: 500, isCreated: false, contact: 'natanael rio wijaya 08971041460' })
-
+    return Response.json({
+        status: 500,
+        isCreated: false,
+        contact: "natanael rio wijaya 08971041460",
+    });
 }
