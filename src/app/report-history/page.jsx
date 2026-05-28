c2372e1271713144aeb90d583612ccbd37d6f7f3
Natanael Rio Wijaya <gafterlife@gmail.com>
2026-05-28 14:11:23 +0700

    rio

--- src/app/report-history/page.jsx
+++ src/app/report-history/page.jsx
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
-import ReportHistory from "@/components/report-history/reportHistory";
+import ReportHistory from "@/components/reportHistory/reportHistory";
 
 export const dynamic = 'force-dynamic'
 
